db93c78d94ea08600019eb5cc7c7b0bfa1ea8e12
Laura <laura@pre1ude.dev>
2026-07-19 00:06:02 +0200

    chore: add wrangler config for cloudflare deployments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,40 +2,44 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        starlight({
-            title: "Cli-Aware Docs",
-            social: [
-                {
-                    icon: "github",
-                    label: "Library",
-                    href: "https://github.com/Pre1ude0/cli-aware",
-                },
-                {
-                    icon: "document",
-                    label: "Docs",
-                    href: "https://github.com/Pre1ude0/cli-aware-docs",
-                },
-            ],
-            sidebar: [
-                {
-                    label: "Guides",
-                    items: [
-                        // Each item here is one entry in the navigation menu.
-                        { label: "Introduction", slug: "guides/introduction" },
-                        {
-                            label: "Getting started",
-                            slug: "guides/getting-started",
-                        },
-                    ],
-                },
-                {
-                    label: "Reference",
-                    items: [{ autogenerate: { directory: "reference" } }],
-                },
-            ],
-        }),
-    ],
+  integrations: [
+      starlight({
+          title: "Cli-Aware Docs",
+          social: [
+              {
+                  icon: "github",
+                  label: "Library",
+                  href: "https://github.com/Pre1ude0/cli-aware",
+              },
+              {
+                  icon: "document",
+                  label: "Docs",
+                  href: "https://github.com/Pre1ude0/cli-aware-docs",
+              },
+          ],
+          sidebar: [
+              {
+                  label: "Guides",
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: "Introduction", slug: "guides/introduction" },
+                      {
+                          label: "Getting started",
+                          slug: "guides/getting-started",
+                      },
+                  ],
+              },
+              {
+                  label: "Reference",
+                  items: [{ autogenerate: { directory: "reference" } }],
+              },
+          ],
+      }),
+  ],
+
+  adapter: cloudflare(),
 });

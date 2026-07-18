4245f2a7c3e78730d40fa7147650ea9feb840794
Laura <laura@pre1ude.dev>
2026-07-18 23:59:37 +0200

    chore: remove default README

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,41 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import mdx from '@astrojs/mdx'
-import preact from '@astrojs/preact'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
-
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), preact(), react(), sitemap()],
-  base: `/`,
-  site: `https://advanced-astro.dev`
-})
+    integrations: [
+        starlight({
+            title: "Cli-Aware Docs",
+            social: [
+                {
+                    icon: "github",
+                    label: "Library",
+                    href: "https://github.com/Pre1ude0/cli-aware",
+                }
+                {
+                    icon: "document",
+                    label: "Docs",
+                    href: "https://github.com/Pre1ude0/cli-aware-docs",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "Guides",
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: "Introduction", slug: "guides/introduction" },
+                        {
+                            label: "Getting started",
+                            slug: "guides/getting-started",
+                        },
+                    ],
+                },
+                {
+                    label: "Reference",
+                    items: [{ autogenerate: { directory: "reference" } }],
+                },
+            ],
+        }),
+    ],
+});
